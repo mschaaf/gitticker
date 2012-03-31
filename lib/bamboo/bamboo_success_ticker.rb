9f9d22456397137e23ed2749a3fff3f1c491c92b
Marko Bauhardt <mb@datameer.com>
2012-03-31 21:03:20 +0200

    add view to show bamboo success and fail plan's

---
+++ lib/bamboo/bamboo_success_ticker.rb
@@ -0,0 +1,52 @@
+require 'yaml'
+require_relative '../http/fetcher'
+require_relative 'bamboo_projects'
+require_relative 'bamboo_results'
+require_relative 'bamboo_result'
+require 'colorize'
+
+class BambooTicker
+
+  def tick
+    #load properties
+    dir = File.dirname(__FILE__)
+    props = YAML::load(File.open(File.join(dir,"bamboo.properties")))
+
+    #fetch content
+    f = Fetcher.new
+
+
+    resultsAsJson = f.fetch(props['bamboo.host'] + BambooResults.urlThatDescribeMe) { |httpAuth|
+                    httpAuth.auth(props['bamboo.user'], props['bamboo.password'])
+    }
+    results = BambooResults.new(resultsAsJson)
+
+    projectsAsJson = f.fetch(props['bamboo.host'] + BambooProjects.urlThatDescribeMe) { |httpAuth|
+              httpAuth.auth(props['bamboo.user'], props['bamboo.password'])
+    }
+    b = BambooProjects.new(projectsAsJson)
+
+    b.projects.each { |project|
+      puts project.key
+      puts '---------------------'
+      project.plans.each { |plan|
+
+        unless  results.result(project.key, plan.key).nil?
+          if(results.result(project.key, plan.key).isSuccessful)
+            print plan.key.colorize(:green)
+            if plan.isBuilding
+               print '*'.colorize(:cyan)
+            end
+            print ' '
+          end
+        end
+
+      }
+      puts ''
+      puts ''
+    }
+  end
+
+end
+
+BambooTicker.new.tick
